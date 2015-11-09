b0111bf759f2c04b9f1acd21610826bf76251180
Tim Hoff <timothy.hoff@praeses.com>
2015-11-09 15:19:54 -0600

    Revert "removing unused core.matrix"
    This reverts commit ed3a2db134060b1449395fd192d1c114799b5c96.

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
  :dependencies [[org.clojure/clojure "1.8.0-beta2"]
                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
   							[criterium "0.4.3"]
+                [net.mikera/vectorz-clj "0.36.0"]
+								[net.mikera/core.matrix "0.42.1"]
         				[incanter/incanter "1.9.0"]]
   :plugins [[lein-codox "0.9.0"]]
   :main ^:skip-aot ann.core
